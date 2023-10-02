8c4f158b52395e72ff97aed32571327f7c71acc6
jagadeeshkoti <jagadeeshkoti96@gmail.com>
2023-10-02 12:22:15 +0530

    commit1

---
+++ src/main/java/package1/test.java
@@ -0,0 +1,14 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class test {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("hii");
+	}
+	
+	
+}
---
+++ src/test/java/package2/test2.java
@@ -0,0 +1,13 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class test2 {
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("hello");
+	}
+
+}
